3ca5dad22e13776514e4279538512707703e6e8d
UmarEngginering <silfha342@gmail.com>
2026-03-24 08:14:19 +0700

    menambahkan AppBar dan profil user

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,26 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFF5F7F9),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF014A94),
+        elevation: 0,
+        toolbarHeight: 80,
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text("Selamat Datang,", style: TextStyle(fontSize: 14, color: Colors.white70)),
+            Text("Umar Faruq", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white)),
+          ],
+        ),
+        actions: [
+          IconButton(icon: const Icon(Icons.notifications_none, color: Colors.white), onPressed: () {}),
+          const Padding(
+            padding: EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(backgroundColor: Colors.lightBlueAccent, child: Text("UF",style:
+            TextStyle(color: Colors.white, fontWeight: FontWeight.bold))),
+          ),
+        ],
+      ),
       body: Center(child: Text("Loading...")), // Placeholder sementara
     );
   }
